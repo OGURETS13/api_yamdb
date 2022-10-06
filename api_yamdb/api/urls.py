dabde616a3797f9256f11a2d33798164bf442ce8
Артур Николаев <artur230398@mail.ru>
2022-10-06 15:32:28 +0300

    review, comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import GetTokenView, SendConfirmationCodeView
+from .views import (GetTokenView, SendConfirmationCodeView, CommentViewSet,
+                    ReviewViewSet)
 
+router = SimpleRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('auth/signup/', SendConfirmationCodeView.as_view()),
+    path('', include(router.urls)),
     path(
         'auth/token/',
         GetTokenView.as_view(),

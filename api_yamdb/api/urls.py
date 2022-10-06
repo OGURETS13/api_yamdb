77ea4718b106193510f9887a4ff3c0c2ac195ebb
Артур Николаев <artur230398@mail.ru>
2022-10-06 18:54:36 +0300

    reviews, comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
 from .views import (GetTokenView, SendConfirmationCodeView, CommentViewSet,
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,12 +2,13 @@ from random import randint
 
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from reviews.models import Comment, Review, User
+from .permissions import AdminModeratorAuthorPermission
+from reviews.models import Review, User
 from .serializers import CommentSerializer, ReviewSerializer, UserSerializer
 
 

4153c511c278453e6f61348428feeb5789580f8f
Артур Николаев <artur230398@mail.ru>
2022-10-10 19:58:25 +0300

    fake commit

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -16,12 +16,12 @@ router.register('categories', CategoryViewSet)
 router.register('genres', GenreViewSet)
 router.register('titles', TitleViewSet)
 # router.register(
-#     r'titles/(?P<title_id>\d+)/reviews',
+#     r'titles/(?P<title_id>\d+)/reviews/',
 #     ReviewViewSet,
 #     basename='reviews'
 # )
 # router.register(
-#     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+#     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/',
 #     CommentViewSet,
 #     basename='comments'
 # )
